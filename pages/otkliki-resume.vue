6b2fee2d1293a2feb1dc91b5815b260991692eee
nice-witch <78809819+nice-witch@users.noreply.github.com>
2025-05-22 18:29:40 +0500

    add: block icons

--- pages/otkliki-resume.vue
+++ pages/otkliki-resume.vue
@@ -1,8 +1,14 @@
 <template>
-  <p>{{resumeData}}</p>
+  <div>
+    <UContainer>
+      <IconsBlock :icons="icons" class-name-wrapper="flex gap-2"/>
+    </UContainer>
+  </div>
 </template>
 
 <script setup lang="ts">
+import IconsBlock from "~/components/resume/IconsBlock.vue";
+
 interface Resume {
   id: number;
   name: string;
@@ -21,6 +27,16 @@ interface Resume {
 
 const RESUME_LINK = 'https://dev.jobcart.ru/wp-json/test/v2/app';
 const resumeData = ref<Resume>();
+const icons = [
+  'material-symbols:print-rounded',
+  'tabler:file-type-pdf',
+  'tabler:file-type-docx',
+  'fa6-solid:file-signature',
+  'streamline:interface-delete-bin-2-remove-delete-empty-bin-trash-garbage',
+  'mdi:file-send',
+  'weui:like-outlined',
+]
+// console.log(resumeData.value);
 
 onMounted(async () => {
   try {
