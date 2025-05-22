379a80bbfc4860082a8a0c3e5053d0dd31bc399e
nice-witch <78809819+nice-witch@users.noreply.github.com>
2025-05-22 18:29:17 +0500

    add: block for icons for resume

---
+++ components/resume/IconsBlock.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :class="classNameWrapper">
+    <SquareIcons
+        v-for="icon in icons"
+        :key="icon"
+        :name="icon"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import SquareIcons from "~/components/ui/Square-icons.vue";
+
+const props = defineProps<{
+  icons: string[];
+  classNameWrapper?: string,
+}>();
+</script>
+
+<style scoped>
+
+</style>
