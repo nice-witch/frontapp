a323dacc4693028633d6ea0d378f8cbc9da83a0c
nice-witch <78809819+nice-witch@users.noreply.github.com>
2025-05-22 18:01:03 +0500

    add: create icon component

---
+++ components/ui/square-icons.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="icon-wrapper">
+    <UIcon :name="name" :size="30"/>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  name: string;
+};
+const props = defineProps<Props>();
+</script>
+
+<style scoped>
+.icon-wrapper {
+  width: 50px;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: black;
+  background-color: #A1AEBF;
+  border-radius: 4px;
+}
+</style>
