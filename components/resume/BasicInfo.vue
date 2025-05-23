9f70c9b3c4102f68b4c12f9d5eeab0b4dfd543e6
nice-witch <78809819+nice-witch@users.noreply.github.com>
2025-05-24 00:18:14 +0500

    fix: color text for themes and add correct status

--- components/resume/BasicInfo.vue
+++ components/resume/BasicInfo.vue
@@ -5,14 +5,14 @@
     </div>
     <div>
       <div class="flex items-center gap-2 text-2xl font-semibold mb-4">
-        <p class="text-white">{{name}}</p>
+        <p class="dark:text-white">{{name}}</p>
         <UIcon name="ic:twotone-open-in-new" :size="15" class="text-black"/>
       </div>
 
-      <div class="flex text-blue-500 gap-3">
+      <div class="flex text-blue-800 dark:text-blue-500 gap-3">
         <div class="flex items-center">
           <p>
-            <span>Кандидат на вакансию:</span> Жадный благотворитель
+            <span class="text-gray-500 dark:text-gray-400">Кандидат на вакансию:</span> Жадный благотворитель
           </p>
           <UIcon name="ic:twotone-open-in-new" :size="15" class="text-black"/>
         </div>
@@ -20,9 +20,9 @@
         <p>({{responseDate}} Отклик)</p>
       </div>
 
-      <p>Просмотрено</p>
+      <p>{{getStatusText(status)}}</p>
       <p>{{age || 'Возраст не указан'}}</p>
-      <div class="flex items-center text-blue-500">
+      <div class="flex items-center text-blue-800 dark:text-blue-500">
         <UIcon name="noto-v1:telephone-receiver" />
         <p>+{{phone}}</p>
         <div class="ml-4 flex gap-2">
@@ -32,7 +32,7 @@
         </div>
       </div>
 
-      <p class="flex items-center text-cyan-300">
+      <p class="flex items-center text-sky-600 dark:text-cyan-300">
         <UIcon name="noto-v1:envelope" :size="23" />
         {{email}}
       </p>
@@ -41,14 +41,19 @@
 </template>
 
 <script setup lang="ts">
+import {statusMap} from '~/types/Resume'
+
 const props = defineProps<{
   photo?: string;
   name: string;
   phone: string;
   email: string;
   age?: number;
+  status: string;
   responseDate: string;
 }>();
+
+const getStatusText = (status: string): string => statusMap[status];
 </script>
 
 <style scoped>
